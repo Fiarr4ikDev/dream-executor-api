22a64718636e6012027dca38c690ac202dbb5ef0
Fiarr4ikDEV <fiarr4ik@gmail.com>
2024-12-05 15:01:25 +0700

    init commit

---
+++ src/main/java/ru/fiarr4ikdev/dreamexecutorapi/DreamExecutorApiApplication.java
@@ -0,0 +1,13 @@
+package ru.fiarr4ikdev.dreamexecutorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamExecutorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamExecutorApiApplication.class, args);
+    }
+
+}
